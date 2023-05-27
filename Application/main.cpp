d983e12072cf8dd5fcba7f47561cab4d08b97dff
Cristian Stanciu <crististanciu095@gmail.com>
2023-05-27 21:11:52 +0300

    Refactor code for main.cpp

--- Application/main.cpp
+++ Application/main.cpp
@@ -11,12 +11,61 @@
 #include "Service.h"
 #include "Constants.h"
 
+#include <memory>
+
+using std::unique_ptr;
+using std::make_unique;
+
+// VARIANTA I
+//typedef enum {
+//    IN_MEMORY_REPO = 0,
+//    FILE_REPO = 1,
+//    DATABASE_REPO = 2
+//} REPO_TYPE;
+
+// VARIANTA II
+enum REPO_TYPE {
+    IN_MEMORY_REPO,
+    FILE_REPO,
+    DATABASE_REPO
+};
+
 void testApp()
 {
     const TestingApp testing_app;
     testing_app.runTestsApp();
 
     qDebug() << "\n--------------------All tests passed--------------------\n";
+}
+
+unique_ptr<AbstractRepo> repoFactory(REPO_TYPE repo_type) {
+    if (repo_type == REPO_TYPE::IN_MEMORY_REPO)
+        return make_unique<RepoProducts>();
+
+    if (repo_type == REPO_TYPE::FILE_REPO)
+    {
+        const string filename{ "produse" };
+        return make_unique<FileRepoProducts>(filename);
+    }
+    
+    // VARIANTA I
+    /*if (repo_type == REPO_TYPE::DATABASE_REPO)
+    {
+        const auto server{ DBConstants::server };
+        const auto username{ DBConstants::username };
+        const auto password{ DBConstants::password };
+        const auto database{ DBConstants::database };
+        const auto table{ DBConstants::table };
+        return make_unique<DatabaseRepository>(server, username, password, database, table);
+    }*/
+
+    // VARIANTA II
+    const auto server{ DBConstants::server };
+    const auto username{ DBConstants::username };
+    const auto password{ DBConstants::password };
+    const auto database{ DBConstants::database };
+    const auto table{ DBConstants::table };
+    return make_unique<DatabaseRepository>(server, username, password, database, table);
 }
 
 int main(int argc, char* argv[])
@@ -29,33 +78,27 @@ int main(int argc, char* argv[])
     // Asamblarea si rularea aplicatiei
     {
         QApplication a(argc, argv);
-
-        bool info_tipuri_disable;
-
+        
         // IN MEMORY REPOSITORY
-        //RepoProducts repo{};
-        //info_tipuri_disable = false;
+        //const auto repo_type{ REPO_TYPE::IN_MEMORY_REPO };
+        // ~IN MEMORY REPOSITORY
 
         // FILE REPOSITORY
-        //const string filename{ "produse" };
-        //FileRepoProducts repo{ filename };
-        //info_tipuri_disable = false;
+        //const auto repo_type{ REPO_TYPE::FILE_REPO };
+        // ~FILE REPOSITORY
 
         // DATABASE REPOSITORY
-        const auto server{ DBConstants::server };
-        const auto username{ DBConstants::username };
-        const auto password{ DBConstants::password };
-        const auto database{ DBConstants::database };
-        const auto table{ DBConstants::table };
-        DatabaseRepository repo{ server, username, password, database, table };
-        info_tipuri_disable = true;
-        
+        const auto repo_type{ REPO_TYPE::DATABASE_REPO };
+        // ~DATABASE REPOSITORY
+
+        auto repo{ repoFactory(repo_type) }; // <=> unique_ptr<AbstractRepo> repo{ repoFactory(repo_type) };
+
         ProductValidator valid;
-        CosCumparaturi cos{ repo };
-        Service srv{ repo, valid, cos };
+        CosCumparaturi cos{ *repo };
+        Service srv{ *repo, valid, cos };
         const auto gui{ new GUI{ srv } };
         gui->show();
-        gui->disableInfoTipuri(true);
+        gui->disableInfoTipuri(repo_type == REPO_TYPE::DATABASE_REPO);
 
         exit_code = a.exec();
     }
